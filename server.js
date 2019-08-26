66ba19bb85905bf80463793b3becc4621722d889
Seun Ogedengbe <seun40@gmail.com>
2019-08-26 10:48:27 -0400

    fukkit

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const WebSocket = require("ws");
+'use strict';
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
 const config = {
   chans: {
     0: {
